7458188fd8b55f33d50d132ae0495b1fd676bec3
Nabeel <nskhan84@gmail.com>
2019-07-23 22:57:54 -0700

    solved some questions

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,8 @@
 function addBorder(picture: string[]): string[] {
-
+    let row: string = '*'.repeat(picture[0].length + 2);
+    return [row, ...picture.map(s => { 
+        return '*'.concat(s, '*')
+    }), row]
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
